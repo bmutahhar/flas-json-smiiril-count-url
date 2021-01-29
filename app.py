dc1ac3fae1fe7fcc07eb4e593c73d2f902120759
bmutahhar <mutahharbinmuzaffar@gmail.com>
2021-01-30 02:16:51 +0500

    Added application/json check

--- app.py
+++ app.py
@@ -17,19 +17,23 @@ def index():
         print("Status Code: ", resp.status_code)
         print(resp.headers)
         print("*" * 100)
-        data = json.loads(resp.text)
-        print("*" * 100)
-        print(data)
-        print("*" * 100)
-        try:
-            number = data['data']['user']['edge_followed_by']['count']
-        except KeyError:
-            number = None
-        if number is not None:
-            return Response(response=json.dumps({'number': number}), status=200,
-                            mimetype='application/json')
+        if resp.headers['Content-Type'].strip() == "application/json":
+            data = json.loads(resp.text)
+            print("*" * 100)
+            print(data)
+            print("*" * 100)
+            try:
+                number = data['data']['user']['edge_followed_by']['count']
+            except KeyError:
+                number = None
+            if number is not None:
+                return Response(response=json.dumps({'number': number.strip()}), status=200,
+                                mimetype='application/json')
+            else:
+                return Response(response=json.dumps({'error': "Could not found count in the API"}), status=500,
+                                mimetype='application/json')
         else:
-            return Response(response=json.dumps({'error': "Could not found count in the API"}), status=500,
+            return Response(response=json.dumps({'error': "Instagram API not returning valid JSON"}), status=500,
                             mimetype='application/json')
     except Exception as e:
         print("*" * 100)
